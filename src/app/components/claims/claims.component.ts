02d57c0368c2400aefa19e6acb2cf00252c6daa1
Jhonatan Alejandro Muñoz Serna <jhotams.developer@gmail.com>
2024-07-11 10:13:26 -0500

    FIX: Ajustes en el component de claims

--- src/app/components/claims/claims.component.ts
+++ src/app/components/claims/claims.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Claim } from '../../models/claim';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-claims',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './claims.component.html',
   styleUrl: './claims.component.scss'
 })
